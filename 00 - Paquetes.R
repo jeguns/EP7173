485694b1e4380b96e204875e7e538f37f86e4b1c
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2022-05-16 15:58:53 -0500

    16/05

---
+++ 00 - Paquetes.R
@@ -0,0 +1,32 @@
+install.packages("pacman")
+library(pacman)
+p_load(caret, lubridate, car, fastDummies, GGally)
+p_load(
+  readr,
+  readxl,
+  writexl,
+  tidyr,
+  skimr,
+  dplyr,
+  naniar,
+  mice,
+  VIM,
+  ggplot2,
+  gam,
+  RBtest,
+  tidymodels,
+  norm,
+  cat,
+  purrr
+)
+p_load(norm2, forecast, mix, matrixStats, ClustImpute)p_load(dplyr,
+                                                             psych,
+                                                             ggplot2,
+                                                             univOutl,
+                                                             purrr,
+                                                             aplpack,
+                                                             tidyr,
+                                                             ggstatsplot)
+p_load(outliers, EnvStats, mvoutlier, DescTools)
+install.packages("ForImp")
+library(ForImp)
